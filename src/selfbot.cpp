02172e2edcb0d428358ac89ef604e7628473d83e
Will Eccles <ghostyx123@gmail.com>
2018-02-09 16:20:15 -0500

    added simple math command

--- src/selfbot.cpp
+++ src/selfbot.cpp
@@ -6,17 +6,30 @@
 #include <cstdio>
 #include <vector>
 #include <regex>
+#include <iterator>
+#include <cmath>
 
-#define REGEX_OPTIONS std::regex_constants::icase|std::regex_constants::optimize
+#define REGEX_FLAGS std::regex_constants::icase|std::regex_constants::optimize
 
 const unsigned short PORT = 6667;
 const char* ADDRESS = "irc.chat.twitch.tv";
 const char* REALNAME = "Will Eccles";
 std::vector<std::string> channels; // this will house all the channels we wish to join
-const std::regex greetingPattern("^((hello|heyo*?|sup|howdy|what('?s)? up|hi)!*\\s+@?(cactus|cacus|cacy|will|tiny_cactus|tiny|ghosty)\\s*!*)", REGEX_OPTIONS); // TODO: deal with emotes and strange greetings
-const std::regex packetsCommand("^?gibpackets", REGEX_OPTIONS); // should only work in #barbaricmustard
-const std::regex mathCommand("^\\?\\s*(-?\\d+(\\.\\d+)?)\\s*([-*+/^%]|XOR|AND|NOT|OR)\\s*(-?\\d+(\\.\\d+)?)\\s*$", REGEX_OPTIONS); // matches math expressions
-const std::regex sqrtCommand("^\\?\\s*sqrt\\s*((\\d+(\\.\\d+)?)|(\\(\\s*\\d+(\\.\\d+)?\\s*\\)))$", REGEX_OPTIONS); // parses ?sqrt command which square roots the given input
+const std::regex greetingPattern("^((hello|heyo*?|sup|howdy|what('?s)? up|hi)!*\\s+@?(cactus|cacus|cacy|will|tiny_cactus|tiny|ghosty)\\s*!*)", REGEX_FLAGS); // TODO: deal with emotes and strange greetings
+const std::regex packetsCommand("^\\?gibpackets", REGEX_FLAGS); // should only work in #barbaricmustard
+const std::regex mathCommand("^\\?\\s*(-?\\d+(\\.\\d+)?)\\s*([-*+/^%]|XOR|AND|NOT|OR)\\s*(-?\\d+(\\.\\d+)?)\\s*$", REGEX_FLAGS); // matches math expressions
+const std::regex sqrtCommand("^\\?\\s*sqrt\\s*((\\d+(\\.\\d+)?)|(\\(\\s*\\d+(\\.\\d+)?\\s*\\)))$", REGEX_FLAGS); // parses ?sqrt command which square roots the given input
+
+// these are used for parsing, but constructed here so they aren't a colossal waste of memory and speed
+std::regex cleanCommand("[\\s?]", REGEX_FLAGS);
+std::regex mathParts("(-?\\d+(\\.\\d+)?)|([-*+/^])", REGEX_FLAGS);
+
+void toLower(std::string& str) {
+	for (size_t i = 0; i < str.size(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += ('a' - 'A');
+	}
+}
 
 // when the server is connected to
 void event_connect(irc_session_t* session, const char * event, const char * origin, const char ** params, unsigned int count) {
@@ -45,12 +58,49 @@ void event_channel(irc_session_t* session, const char * event, const char * orig
 	
 	// send a message in the channel:
 	// irc_cmd_msg(session, params[0], text);
-	if ( std::regex_match(message, packetsCommand)) {
+	if (std::regex_match(message, packetsCommand)) {
 		// this will give everyone my packets
 		if (channel == "#barbaricmustard") {
 			irc_cmd_msg(session, params[0], std::string("!giveall " + nick).c_str());
 			std::cout << "Gave all packets to " << nick << '\n';
 		}
+	} else if (std::regex_match(message, mathCommand)) {
+		// parse the math expression given to the command and then do the math and put it in chat
+		std::regex_replace(message, cleanCommand, "");
+		// now that the string has no whitespace or ? at the beginning, parse the math
+		// it should now be in the format of <+/-number><operation><+/-number>
+		// use a regex iterator to find the parts
+		std::vector<std::string> parts;
+		auto words_begin = std::sregex_iterator(message.begin(), message.end(), mathParts);
+		auto words_end = std::sregex_iterator();
+		for (std::sregex_iterator i = words_begin; i != words_end; i++) {
+			std::smatch match = *i;
+			parts.push_back(match.str());
+		}
+		toLower(parts[1]);
+
+		// DEBUG:
+		//for (auto s: parts)
+		//	std::cout << s << '\n';
+		
+		// this is the part where we make it do math
+		long double n1 = std::stold(parts[0]);
+		long double n2 = std::stold(parts[2]);
+		long double r;
+		std::string op = parts[1];
+		if (op == "+") {
+			r = n1 + n2;
+		} else if (op == "-") {
+			r = n1 - n2;
+		} else if (op == "/") {
+			r = n1 / n2;
+		} else if (op == "*") {
+			r = n1 * n2;
+		} else if (op == "^") {
+			r = pow(n1, n2);
+		}
+		
+		irc_cmd_msg(session, params[0], std::string("/me " + nick + "-> " + std::to_string(r)).c_str());
 	}
 }
 
