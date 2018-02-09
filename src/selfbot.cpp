131b6e23eb76a1d947b384615e5b264192ab1ca8
Will Eccles <ghostyx123@gmail.com>
2018-02-09 12:42:53 -0500

    added some commands

--- src/selfbot.cpp
+++ src/selfbot.cpp
@@ -7,10 +7,16 @@
 #include <vector>
 #include <regex>
 
+#define REGEX_OPTIONS std::regex_constants::icase|std::regex_constants::optimize
+
 const unsigned short PORT = 6667;
 const char* ADDRESS = "irc.chat.twitch.tv";
 const char* REALNAME = "Will Eccles";
 std::vector<std::string> channels; // this will house all the channels we wish to join
+const std::regex greetingPattern("^((hello|heyo*?|sup|howdy|what('?s)? up|hi)!*\\s+@?(cactus|cacus|cacy|will|tiny_cactus|tiny|ghosty)\\s*!*)", REGEX_OPTIONS); // TODO: deal with emotes and strange greetings
+const std::regex packetsCommand("^?gibpackets", REGEX_OPTIONS); // should only work in #barbaricmustard
+const std::regex mathCommand("^\\?\\s*(-?\\d+(\\.\\d+)?)\\s*([-*+/^%]|XOR|AND|NOT|OR)\\s*(-?\\d+(\\.\\d+)?)\\s*$", REGEX_OPTIONS); // matches math expressions
+const std::regex sqrtCommand("^\\?\\s*sqrt\\s*((\\d+(\\.\\d+)?)|(\\(\\s*\\d+(\\.\\d+)?\\s*\\)))$", REGEX_OPTIONS); // parses ?sqrt command which square roots the given input
 
 // when the server is connected to
 void event_connect(irc_session_t* session, const char * event, const char * origin, const char ** params, unsigned int count) {
@@ -35,9 +41,17 @@ void event_channel(irc_session_t* session, const char * event, const char * orig
 	char nickbuff[128];
 	irc_target_get_nick(origin, nickbuff, sizeof(nickbuff));
 	nick = std::string(nickbuff);
+	std::string channel(params[0]);
 	
 	// send a message in the channel:
 	// irc_cmd_msg(session, params[0], text);
+	if ( std::regex_match(message, packetsCommand)) {
+		// this will give everyone my packets
+		if (channel == "#barbaricmustard") {
+			irc_cmd_msg(session, params[0], std::string("!giveall " + nick).c_str());
+			std::cout << "Gave all packets to " << nick << '\n';
+		}
+	}
 }
 
 int main(void) {
